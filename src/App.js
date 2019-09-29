4ec2532f9bdc6eff8d521d77fd32eb9ea603763e
regg1e <706648109@qq.com>
2019-09-29 17:29:15 +0800

    引入了antd 集成了路由

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React, { Component } from 'react';
+import {BrowserRouter,Route,Switch} from 'react-router-dom'
 
+import Login from './pages/login/login'
+import Admin from './pages/admin/admin'
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {  }
-  }
+
   render() { 
-    return (<div> hello world </div>  );
+    return (
+      <BrowserRouter>
+        <Switch> {/* 只匹配一个  */}
+          <Route path='/login' component={Login}></Route>
+          <Route path="/" component={Admin}></Route>
+        </Switch>
+        
+      </BrowserRouter> 
+    );
   }
 }
  
